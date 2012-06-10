2b03ae48761b6a71f06dc77105a781659c38fac9
Foo Bar <foobar22@hotmail.com>
2012-06-09 22:47:38 -0700

    Fixing couple of build issues.

---
+++ Sunlight.Framework/Observables/AttachedProperty.cs
@@ -0,0 +1,31 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="AttachedProperty.cs" company="">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Sunlight.Framework.Observables
+{
+    using System;
+
+    /// <summary>
+    /// Definition for AttachedProperty
+    /// </summary>
+    public class AttachedProperty<T>
+    {
+        private string name;
+
+        internal AttachedProperty(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+    }
+}
