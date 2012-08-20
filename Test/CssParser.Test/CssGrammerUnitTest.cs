9d44dcc83aada54628539d8ae9e5cd4b8e2a38ef
foobar22 <foobar22@hotmail.com>
2012-08-20 16:26:16 -0700

    Fixed bunch of more build issues.

--- Test/CssParser.Test/CssGrammerUnitTest.cs
+++ Test/CssParser.Test/CssGrammerUnitTest.cs
@@ -16,10 +16,10 @@
             Assert.IsInstanceOfType<CssClass>(grammer.Rules[0].Selectors[0]);
 
             Assert.AreEqual(1, grammer.Rules[0].Properties.Count);
-            Assert.AreEqual("border", grammer.Rules[0].Properties[0].propertyName);
+            Assert.AreEqual("border", grammer.Rules[0].Properties[0].PropertyName);
             Assert.AreElementsNotSame(
                 new string[] { "#ddeeff", "thin", "1px", "solid" },
-                grammer.Rules[0].Properties[0].propertyArgs);
+                grammer.Rules[0].Properties[0].PropertyArgs);
         }
     }
 }
