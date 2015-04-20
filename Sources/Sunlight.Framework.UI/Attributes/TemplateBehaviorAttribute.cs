837049c9712c81e54b6979670e81eea724123dc9
Foo Bar <foobar22@hotmail.com>
2015-04-20 12:55:44 -0700

    Adding TemplateBehaviour attribute

---
+++ Sources/Sunlight.Framework.UI/Attributes/TemplateBehaviorAttribute.cs
@@ -0,0 +1,24 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TemplateBehaviorAttribute.cs" company="">
+//     Copyright (c) . All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Sunlight.Framework.UI.Attributes
+{
+    using System;
+
+    /// <summary>
+    /// Definition for TemplateBehaviorAttribute
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    public class TemplateBehaviorAttribute
+    {
+        private string behaviorName;
+        public TemplateBehaviorAttribute(string behaviorName)
+        { this.behaviorName = behaviorName; }
+
+        public string BehaviorName
+        { get { return this.behaviorName; } }
+    }
+}
