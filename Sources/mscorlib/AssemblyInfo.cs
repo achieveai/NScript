fc67887da0b65b8036b49bd07a7acd02990d5da5
Foo Bar <foo@bar.com>
2017-12-13 10:43:03 -0800

    Changes to help build with latest VS changes.

--- Sources/mscorlib/AssemblyInfo.cs
+++ Sources/mscorlib/AssemblyInfo.cs
@@ -1,6 +1,6 @@
-﻿// Assembly mscorlib, Version 0.1.1.0
+﻿// Assembly mscorlib, Version 2.1.1.0
 
-[assembly: System.Reflection.AssemblyVersion("0.1.1.0")]
+[assembly: System.Reflection.AssemblyVersion("2.1.1.0")]
 [assembly: System.Runtime.Versioning.TargetFramework(".NETFramework,Version=v2.0", FrameworkDisplayName=".NET Framework 2")]
 [assembly: System.Runtime.CompilerServices.ScriptAssembly("corlib")]
 [assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9 2010")]
@@ -9,7 +9,7 @@
 [assembly: System.Reflection.AssemblyCompany("WebApps.Net")]
 [assembly: System.Reflection.AssemblyProduct("WebApps mscorlib")]
 [assembly: System.Reflection.AssemblyTrademark("")]
-[assembly: System.Reflection.AssemblyFileVersion("0.1.1.0")]
+[assembly: System.Reflection.AssemblyFileVersion("2.1.1.0")]
 [assembly: System.CLSCompliant(false)]
 [assembly: System.Reflection.AssemblyTitle("mscorlib")]
 
