d58b102d528663748501d055e63250e4a20271c9
Gautam <g@b.com>
2018-06-10 13:48:58 -0700

    Adding support for LazyAsync and TryResolve

--- Sources/Sunlight.Framework/IoC/IocContainer.cs
+++ Sources/Sunlight.Framework/IoC/IocContainer.cs
@@ -34,24 +34,25 @@ namespace Sunlight.Framework
         [Script(@"
             if (!this.@{[Sunlight.Framework]Sunlight.Framework.TypeRegistry::isStatic})
             {
-                return this.@{[Sunlight.Framework]Sunlight.Framework.TypeRegistry::factory}();
+                return this.@{[Sunlight.Framework]Sunlight.Framework.TypeRegistry::factory}(ioc);
             }
 
             if (!this.@{[Sunlight.Framework]Sunlight.Framework.TypeRegistry::isCreated})
             {
-                this.@{[Sunlight.Framework]Sunlight.Framework.TypeRegistry::value} = this.@{[Sunlight.Framework]Sunlight.Framework.TypeRegistry::factory}();
+                this.@{[Sunlight.Framework]Sunlight.Framework.TypeRegistry::value} = this.@{[Sunlight.Framework]Sunlight.Framework.TypeRegistry::factory}(ioc);
                 this.@{[Sunlight.Framework]Sunlight.Framework.TypeRegistry::isCreated} = true;
             }
 
             return this.@{[Sunlight.Framework]Sunlight.Framework.TypeRegistry::value};
             ")]
         [IgnoreGenericArguments]
-        public extern T GetValue<T>() where T : class;
+        public extern T GetValue<T>(IocContainer ioc) where T : class;
     }
 
     public class IocContainer
     {
         StringDictionary<TypeRegistry> factoryMap = new StringDictionary<TypeRegistry>();
+        StringDictionary<TypeRegistry> asyncFactoryMap = new StringDictionary<TypeRegistry>();
         int resolutionCount = 0;
 
         public IocHelper Register<T>(Func<T> factory) where T : class
@@ -69,17 +70,59 @@ namespace Sunlight.Framework
                 });
         }
 
+        public IocHelper Register<T>(Func<IocContainer, T> factory) where T : class
+        {
+            TypeRegistry typeRegistry = new TypeRegistry(factory);
+            this.factoryMap[typeof(T).TypeId] = typeRegistry;
+            return new IocHelper(
+                delegate
+                {
+                    typeRegistry.IsStatic = true;
+                },
+                delegate(Type type)
+                {
+                    this.factoryMap[type.TypeId] = typeRegistry;
+                });
+        }
+
+        public IocHelper RegisterAsync<T>(Func<IocContainer, Promise<T>> asyncFactory) where T : class
+        {
+            Func<IocContainer, LazyAsync<T>> lazyAsyncFactory = (ioc) => new LazyAsync<T>(asyncFactory(ioc));
+            TypeRegistry typeRegistry = new TypeRegistry(lazyAsyncFactory);
+            this.asyncFactoryMap[typeof(T).TypeId] = typeRegistry;
+            return new IocHelper(
+                delegate
+                {
+                    typeRegistry.IsStatic = true;
+                },
+                delegate(Type type)
+                {
+                    this.asyncFactoryMap[type.TypeId] = typeRegistry;
+                });
+        }
+
         public T Resolve<T>() where T : class
         {
+            var rv = this.TryResolve<T>();
+            if (rv == null)
+            { throw new Exception("type not registreed"); }
+
+            return rv;
+        }
+
+        public T TryResolve<T>() where T : class
+        {
             if (this.resolutionCount > 100)
-            {
-                throw new Exception("Ioc has cycles, use ResolveLazy<T> to break cycle");
-            }
+            { throw new Exception("Ioc has cycles, use ResolveLazy<T> to break cycle"); }
 
             this.resolutionCount++;
             try
             {
-                return this.factoryMap[typeof(T).TypeId].GetValue<T>();
+                TypeRegistry typeRegistry = null;
+                if (this.factoryMap.TryGetValue(typeof(T).TypeId, out typeRegistry))
+                { return typeRegistry.GetValue<T>(this); }
+
+                return null;
             }
             finally
             {
@@ -87,33 +130,52 @@ namespace Sunlight.Framework
             }
         }
 
-        public object ResolveType(Type type)
+        public LazyAsync<T> ResolveAsyn<T>() where T : class
+        {
+            var rv = this.TryResolveAsync<T>();
+            if (rv == null)
+            { throw new Exception("type not registreed"); }
+
+            return rv;
+        }
+
+        public LazyAsync<T> TryResolveAsync<T>()
         {
             if (this.resolutionCount > 100)
-            {
-                throw new Exception("Ioc has cycles, use ResolveLazy<T> to break cycle");
-            }
-
+            { throw new Exception("Ioc has cycles, use ResolveLazy<T> to break cycle"); }
 
             this.resolutionCount++;
             try
             {
-                return this.factoryMap[type.TypeId].GetValue<object>();
+                TypeRegistry typeRegistry = null;
+                if (this.factoryMap.TryGetValue(typeof(T).TypeId, out typeRegistry))
+                { return typeRegistry.GetValue<LazyAsync<T>>(this); }
+
+                return null;
             }
             finally
             {
                 this.resolutionCount--;
             }
+        }
+
+        public Lazy<T> ResolveLazyOrNull<T>() where T : class
+        {
+            TypeRegistry typeRegistry = null;
+            if (this.factoryMap.TryGetValue(typeof(T).TypeId, out typeRegistry))
+            { return new Lazy<T>(() => typeRegistry.GetValue<T>(this)); }
+
+            return null;
         }
 
         public Lazy<T> ResolveLazy<T>() where T : class
         {
-            return new Lazy<T>(this.factoryMap[typeof(T).TypeId].GetValue<T>);
+            return new Lazy<T>(() => this.factoryMap[typeof(T).TypeId].GetValue<T>(this));
         }
 
         public Factory<T> ResolveFactory<T>() where T : class
         {
-            return new Factory<T>(this.factoryMap[typeof(T).TypeId].GetValue<T>);
+            return new Factory<T>(() => this.factoryMap[typeof(T).TypeId].GetValue<T>(this));
         }
     }
 }
